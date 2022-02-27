3f7925fbd6235823bc2e2e0227d017a9ad395895
2lcm <osn011200@naver.com>
2022-02-27 20:14:23 +0900

    start.spring.io

---
+++ src/main/java/afoc/snsclonespringboot/SnsCloneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package afoc.snsclonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCloneSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCloneSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/afoc/snsclonespringboot/SnsCloneSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package afoc.snsclonespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCloneSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
