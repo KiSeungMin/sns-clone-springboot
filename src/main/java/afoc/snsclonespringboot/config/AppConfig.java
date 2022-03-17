884fc3e4b1f2ed6662214942e5ebdf42b19b88c2
2lcm <osn011200@naver.com>
2022-03-17 19:18:36 +0900

    memory -> jpa

--- src/main/java/afoc/snsclonespringboot/config/AppConfig.java
+++ src/main/java/afoc/snsclonespringboot/config/AppConfig.java
@@ -1,21 +1,27 @@
 package afoc.snsclonespringboot.config;
 
-import afoc.snsclonespringboot.member.MemberRepository;
-import afoc.snsclonespringboot.member.MemberService;
-import afoc.snsclonespringboot.member.MemberServiceImpl;
-import afoc.snsclonespringboot.member.MemoryMemberRepository;
+import afoc.snsclonespringboot.member.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Persistent;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class AppConfig {
+    @PersistenceContext
+    private EntityManager em;
+
     @Bean
     public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository());
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
