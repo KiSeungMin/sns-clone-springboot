1f4ab86d80c21fa104a51426cb6b947a6bafcf9f
2lcm <osn011200@naver.com>
2022-03-20 16:39:24 +0900

    follow test

--- src/main/java/afoc/snsclonespringboot/config/AppConfig.java
+++ src/main/java/afoc/snsclonespringboot/config/AppConfig.java
@@ -33,8 +33,8 @@ public class AppConfig {
 
     @Bean
     public FollowRepository followRepository() {
-        return new MemoryFollowRepository();
-//        return new JpaFollowRepository(em);
+//        return new MemoryFollowRepository();
+        return new JpaFollowRepository(em);
     }
 
     @Bean
