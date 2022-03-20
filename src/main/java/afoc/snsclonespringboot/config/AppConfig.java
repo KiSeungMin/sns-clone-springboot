9b1266b967501d07f8c8a937d3f9e3d6eda2460e
2lcm <osn011200@naver.com>
2022-03-20 16:37:46 +0900

    follow test

--- src/main/java/afoc/snsclonespringboot/config/AppConfig.java
+++ src/main/java/afoc/snsclonespringboot/config/AppConfig.java
@@ -22,7 +22,7 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository());
+        return new MemberServiceImpl(memberRepository(), followRepository());
     }
 
     @Bean
