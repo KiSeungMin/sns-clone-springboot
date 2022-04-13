52892f9f77d478335b88f05b9870a1eebe7c72f4
KiSeungMin <ksm011212@gmail.com>
2022-04-13 19:10:15 +0900

    add LikeForm

---
+++ src/main/java/afoc/snsclonespringboot/board/like/LikeForm.java
@@ -0,0 +1,12 @@
+package afoc.snsclonespringboot.board.like;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LikeForm {
+
+    private Long boardId;
+    private Long memberId;
+
+}
